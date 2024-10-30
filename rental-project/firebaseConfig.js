f8e79926dfcbd16324ed2476f74e49d41c68dcd3
Kamal Qureshi <116005089+kamal-qureshi@users.noreply.github.com>
2024-10-30 16:47:26 +0500

    eas

--- rental-project/firebaseConfig.js
+++ rental-project/firebaseConfig.js
@@ -1,20 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase, goOnline, goOffline } from "firebase/database";
-import 'react-native-get-random-values';
+import "react-native-get-random-values";
 
 const firebaseConfig = {
-  apiKey: "XXXXX_XXXXXXX_XXXXX",
-  authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-  databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxxxx",
-  projectId: "xxxxxxxxxx",
-  storageBucket: "xxxxxxxxxx.xxxxx.com",
-  messagingSenderId: "xxxxxxxxxxx",
-  appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  apiKey: "AIzaSyApj4HWYhFq-GW3KpWEd_A_bk3EAs6c22Q",
+  authDomain: "rental-management-system-01.firebaseapp.com",
+  databaseURL:
+    "https://rental-management-system-01-default-rtdb.firebaseio.com",
+  projectId: "rental-management-system-01",
+  storageBucket: "rental-management-system-01.appspot.com",
+  messagingSenderId: "476743866480",
+  appId: "1:476743866480:web:b0d0409bd1ee52ddd8fcb5",
 };
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
-goOnline (database)
+goOnline(database);
 database.persistenceEnabled = true;
 
 export { database, goOffline, goOnline };
